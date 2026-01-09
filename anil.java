5291de6ed0cc3712a440a59b7de4bb320533119d
Your Name <you@example.com>
2026-01-09 11:16:58 +0530

    csea

---
+++ anil.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class anil{
+    public static void main(String[] args) {
+        // Loop from i = 1 to i = 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i); // Print the current value of i
+        }
+    }
+}
+
